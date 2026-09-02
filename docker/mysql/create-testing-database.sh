68b0b970572c8c4fe7e69b3cca9a1695911c84a7
Agris <7924@inbox.lv>
2026-09-02 14:03:00 +0300

    chore: update database configuration and ignore patterns for local development environment

--- docker/mysql/create-testing-database.sh
+++ docker/mysql/create-testing-database.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 mysql --user=root --password="$MYSQL_ROOT_PASSWORD" <<-EOSQL
+    CREATE DATABASE IF NOT EXISTS auditors;
+    CREATE DATABASE IF NOT EXISTS laravel;
     CREATE DATABASE IF NOT EXISTS testing;
+    GRANT ALL PRIVILEGES ON \`auditors%\`.* TO '$MYSQL_USER'@'%';
+    GRANT ALL PRIVILEGES ON \`laravel%\`.* TO '$MYSQL_USER'@'%';
     GRANT ALL PRIVILEGES ON \`testing%\`.* TO '$MYSQL_USER'@'%';
+    FLUSH PRIVILEGES;
 EOSQL
